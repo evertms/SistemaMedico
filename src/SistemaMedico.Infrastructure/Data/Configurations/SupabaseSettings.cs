f029fc3f0e4c783ba511c905df6f3506eaf50b14
Evert Moreno Serrate <evert23122004@gmail.com>
2025-06-24 01:00:09 -0400

    Configuraciones para servicios externos

---
+++ src/SistemaMedico.Infrastructure/Data/Configurations/SupabaseSettings.cs
@@ -0,0 +1,7 @@
+namespace SistemaMedico.Infrastructure.Data.Configurations;
+
+public class SupabaseSettings
+{
+    public string Url { get; set; } = string.Empty;
+    public string AnonKey { get; set; } = string.Empty;
+}
