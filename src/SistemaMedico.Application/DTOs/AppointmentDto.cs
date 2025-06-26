62a9f981a34acb4cadd584e3e10dbec5c19e0fa2
Evert Moreno Serrate <evert23122004@gmail.com>
2025-06-26 11:29:33 -0400

    Create AppointmentDto.cs
    DTO para obtener las consultas pendientes

---
+++ src/SistemaMedico.Application/DTOs/AppointmentDto.cs
@@ -0,0 +1,10 @@
+namespace SistemaMedico.Application.DTOs;
+
+public class AppointmentDto
+{
+    public Guid Id { get; set; }
+    public string DoctorName { get; set; }
+    public DateTime ScheduledDate { get; set; }
+    public string Status { get; set; }
+    public string? ReasonForVisit { get; set; }
+}
